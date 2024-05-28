0c928184c9a738fa74f95b90cb9bb42c2a692f81
Anton Sidorov <antosid@gmail.com>
2024-05-28 23:13:42 +0100

    fix: refactor: Simplify code in src/reader.rs

--- src/reader.rs
+++ src/reader.rs
@@ -39,10 +39,7 @@ pub fn read_lines<R: BufRead>(
                                 .entry(icao)
                                 .and_modify(|p| p.update(&message, df))
                                 .or_insert(Plane::from_message(&message, df, icao));
-                            debug!("Total planes in view: {}", planes.len());
-                            debug!("{}", planes[&icao]);
-                            debug!("{}", planes[&icao]);
-
+                            
                             if now.signed_duration_since(timestamp).num_seconds() > args.refresh {
                                 clear_screen();
                                 print_header();
