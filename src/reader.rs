7d6b060a2c72b30cb1b8a7adfe64868c587bb609
Anton Sidorov <antosid@gmail.com>
2024-05-31 14:12:59 +0100

    display: refactor: Update sorting logic for planes based on order_by field

--- src/reader.rs
+++ src/reader.rs
@@ -192,43 +192,46 @@ fn print_planes(planes: &mut HashMap<u32, Plane>, args: &Args) {
     //let mut reversed_order = args.order_by.iter().collect::<Vec<_>>();
     //reversed_order.reverse();
     for order_by in &args.order_by {
-        match order_by {
-            'a' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.altitude);
+        for c in order_by.chars() {
+            match c {
+                'a' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.altitude);
+                }
+                'A' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.altitude);
+                    planes_vector.reverse();
+                }
+                'c' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.category);
+                }
+                'C' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| {
+                        -(((p.category.0 << 1) | p.category.1) as i32)
+                    });
+                }
+                'N' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.lat as i32);
+                }
+                'S' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| -(p.lat as i32));
+                }
+                'W' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.lon as i32);
+                }
+                'E' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| -(p.lon as i32));
+                }
+                's' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.squawk);
+                }
+                'V' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| -(p.vrate.unwrap_or(0)));
+                }
+                'v' => {
+                    planes_vector.sort_by_cached_key(|&(_, p)| p.vrate.unwrap_or(0));
+                }
+                _ => {}
             }
-            'A' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.altitude);
-                planes_vector.reverse();
-            }
-            'c' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.category);
-            }
-            'C' => {
-                planes_vector
-                    .sort_by_cached_key(|&(_, p)| -(((p.category.0 << 1) | p.category.1) as i32));
-            }
-            'N' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.lat as i32);
-            }
-            'S' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| -(p.lat as i32));
-            }
-            'W' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.lon as i32);
-            }
-            'E' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| -(p.lon as i32));
-            }
-            's' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.squawk);
-            }
-            'V' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| -(p.vrate.unwrap_or(0)));
-            }
-            'v' => {
-                planes_vector.sort_by_cached_key(|&(_, p)| p.vrate.unwrap_or(0));
-            }
-            _ => {}
         }
     }
 
