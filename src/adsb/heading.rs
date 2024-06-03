f67b61cfffb82cfa05694f9f108d3c8745c4e8f2
Anton Sidorov <antosid@gmail.com>
2024-06-03 15:00:42 +0100

    meteo: refactor: Update heading and ground_track functions to return u32 instead of f64

--- src/adsb/heading.rs
+++ src/adsb/heading.rs
@@ -1,6 +1,6 @@
-pub fn heading(message: &[u32]) -> Option<f64> {
+pub fn heading(message: &[u32]) -> Option<u32> {
     if let Some((_, value)) = crate::adsb::flag_and_range_value(message, 1, 47, 56) {
-        Some(((value * 360) >> 10) as f64)
+        Some((value * 360) >> 10)
     } else {
         None
     }
--- src/adsb/track.rs
+++ src/adsb/track.rs
@@ -1,7 +1,7 @@
-pub fn ground_track(message: &[u32]) -> Option<f64> {
+pub fn ground_track(message: &[u32]) -> Option<u32> {
     if let Some((status, value)) = crate::adsb::flag_and_range_value(message, 45, 46, 52) {
         match status {
-            1 => Some(value as f64 * 360.0 / 128.0),
+            1 => Some((value * 360) >> 7),
             _ => None,
         }
     } else {
@@ -9,7 +9,7 @@ pub fn ground_track(message: &[u32]) -> Option<f64> {
     }
 }
 
-pub fn track_and_groundspeed(message: &[u32], is_supersonic: bool) -> (Option<f64>, Option<f64>) {
+pub fn track_and_groundspeed(message: &[u32], is_supersonic: bool) -> (Option<u32>, Option<u32>) {
     let sp_west = match crate::adsb::flag_and_range_value(message, 46, 47, 56) {
         Some((dir_west, speed_west)) => match dir_west {
             1 => -(speed_west as f64 - 1.0),
@@ -26,9 +26,9 @@ pub fn track_and_groundspeed(message: &[u32], is_supersonic: bool) -> (Option<f6
         _ => 0.0,
     };
 
-    let supersonic = |x| if is_supersonic { x * 4.0 } else { x };
-    let groundspeed = supersonic((sp_west.powi(2) + sp_south.powi(2)).sqrt());
-    let track = (sp_west.atan2(sp_south).to_degrees() + 360.0) % 360.0;
+    let supersonic = |x| if is_supersonic { x * 4 } else { x };
+    let groundspeed = supersonic((sp_west.powi(2) + sp_south.powi(2)).sqrt().floor() as u32);
+    let track = (((sp_west as f64).atan2(sp_south).to_degrees().floor() + 360.0) % 360.0) as u32;
     (Some(track), Some(groundspeed))
 }
 
@@ -42,8 +42,8 @@ mod tests {
     fn test_track_and_groundspeed() {
         if let Some(message) = adsb::message("8DC06A75990D0628B0040C8AA788") {
             if let (Some(track), Some(groundspeed)) = track_and_groundspeed(&message, false) {
-                assert_eq!(groundspeed, 416.0492759277439);
-                assert_eq!(track, 321.14662565964665);
+                assert_eq!(groundspeed, 416);
+                assert_eq!(track, 321);
             };
         };
     }
