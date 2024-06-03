5af092293b75bfb9c272a97b96373d688574ebf7
Anton Sidorov <antosid@gmail.com>
2024-06-03 10:14:00 +0100

    ground: refactor: Refactor ground_movement and heading functions to use flag_and_range_value

--- src/adsb/heading.rs
+++ src/adsb/heading.rs
@@ -1,6 +1,7 @@
 pub fn heading(message: &[u32]) -> Option<f64> {
-    Some(
-        ((((message[11] & 3) << 8 | (message[12] & 0xF) << 4 | (message[13] & 0xF)) * 360) >> 10)
-            as f64,
-    )
+    if let Some((_, value)) = crate::adsb::flag_and_range_value(message, 1, 47, 56) {
+        Some(((value * 360) >> 10) as f64)
+    } else {
+        None
+    }
 }
