103a8b7c96f2883b6a19c2463445fe2e1327f204
Anton Sidorov <antosid@gmail.com>
2024-06-04 08:49:14 +0100

    meteo: refactor: Remove unused heading and track functions from adsb module

--- src/adsb/heading.rs
+++
@@ -1,7 +0,0 @@
-pub fn heading(message: &[u32]) -> Option<u32> {
-    if let Some((_, value)) = crate::adsb::flag_and_range_value(message, 1, 47, 56) {
-        Some((value * 360) >> 10)
-    } else {
-        None
-    }
-}
