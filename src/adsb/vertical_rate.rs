b630e67700c066503e99e967bfd55ca6c5a82a14
Anton Sidorov <antosid@gmail.com>
2024-06-10 12:37:46 +0100

    fom: refactor: Separate ADS-B vertical rate calculation into its own function

--- src/adsb/vertical_rate.rs
+++ src/adsb/vertical_rate.rs
@@ -1,14 +1,13 @@
 pub fn vertical_rate(message: &[u32]) -> Option<i32> {
-    if let Some((is_negative, value)) = crate::adsb::flag_and_range_value(message, 69, 70, 78) {
-        let absolute_rate = ((value - 1) << 6) as i32;
-        match absolute_rate {
-            0 => None,
-            _ => match is_negative {
-                1 => Some(-(absolute_rate)),
-                _ => Some(absolute_rate),
-            },
-        }
-    } else {
-        None
+    crate::adsb::flag_and_range_value(message, 69, 70, 78)
+        .filter(|&f| f.0 == 1 && f.1 != 0)
+        .map(|(sign, value)| vertical_rate_value(sign, value))
+}
+
+fn vertical_rate_value(sign: u32, value: u32) -> i32 {
+    let value = ((value - 1) << 6) as i32;
+    match sign {
+        1 => -value,
+        _ => value,
     }
 }
