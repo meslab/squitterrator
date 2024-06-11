817e881d8f39ec2243b257486ab3e00765a5a337
Anton Sidorov <antosid@gmail.com>
2024-06-11 13:01:19 +0100

    fom: refactor: Update ADS-B wind range in BDS 4.4 message parsing

--- src/adsb/bds/bds_4_4/mod.rs
+++ src/adsb/bds/bds_4_4/mod.rs
@@ -1,5 +1,8 @@
+use log::debug;
+
 use crate::adsb;
 
+#[derive(Debug)]
 pub struct Meteo {
     pub temp: Option<f64>,
     pub wind: Option<(u32, u32)>,
@@ -43,7 +46,9 @@ impl Default for Meteo {
 }
 
 pub fn is_bds_4_4(message: &[u32]) -> Option<Meteo> {
-    if adsb::goodflags(message, 37, 38, 55)
+    let fom = adsb::range_value(message, 33, 36).unwrap();
+    if fom > 0b1000
+        && adsb::goodflags(message, 37, 38, 55)
         && adsb::goodflags(message, 37, 57, 66)
         && adsb::goodflags(message, 67, 68, 78)
         && adsb::goodflags(message, 79, 80, 81)
@@ -51,13 +56,19 @@ pub fn is_bds_4_4(message: &[u32]) -> Option<Meteo> {
     {
         let meteo = Meteo::from_data(
             adsb::temperature_4_4(message).filter(|x| (-80.0..=60.0).contains(x)),
-            adsb::wind_4_4(message).filter(|x| (0..=250).contains(&x.0)),
+            adsb::wind_4_4(message).filter(|x| (0..=300).contains(&x.0)),
             adsb::humidity_4_4(message).filter(|x| (0..=100).contains(x)),
             adsb::turbulence_4_4(message).filter(|x| (0..=15).contains(x)),
             adsb::pressure_4_4(message).filter(|x| (0..=2048).contains(x)),
         );
+        debug!(
+            "DF:{} F:{:b} {:?}",
+            adsb::df(message).unwrap(),
+            adsb::range_value(message, 33, 36).unwrap(),
+            meteo
+        );
         if meteo.temp.is_some()
-            && meteo.wind.is_some()
+            // && meteo.wind.is_some()
             && meteo.humidity.is_some()
             && meteo.turbulence.is_some()
             && meteo.pressure.is_some()
