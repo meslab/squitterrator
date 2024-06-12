fb5c4f9ebd39a6cd1227763792880a0226cfd476
Anton Sidorov <antosid@gmail.com>
2024-06-12 13:16:56 +0100

    cleanup: chore: update squitterator dependency to version 0.2.5

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use std::time::Duration;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[clap(
-    version = "v0.2.4",
+    version = "v0.2.5",
     author = "Anton Sidorov tonysidrock@gmail.com",
     about = "ADS-B squitter decoder"
 )]
