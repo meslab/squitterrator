5267ee4279f1a545f6808b441e5ad0f0cfe63ff2
Anton Sidorov <antosid@gmail.com>
2024-06-15 13:17:41 +0100

    refactoring: refactor: Update squitterator version to 0.2.6
    This commit updates the squitterator version from 0.2.5 to 0.2.6 in the Cargo.lock, Cargo.toml, and main.rs files. This update likely includes bug fixes, improvements, or new features in the squitterator library. Keeping the dependencies up to date ensures that the project benefits from the latest enhancements and fixes.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use std::time::Duration;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[clap(
-    version = "v0.2.5",
+    version = "v0.2.6",
     author = "Anton Sidorov tonysidrock@gmail.com",
     about = "ADS-B squitter decoder"
 )]
